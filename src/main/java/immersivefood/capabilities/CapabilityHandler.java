f8dcd9fbd562fa5aa8d3d810dc90e7b0f101f332
Ronald Goedeke <ronald.goedeke@outlook.com>
2018-09-08 18:04:01 +0200

    Added in a food modifier which gets it from the item saturation and heal amount

--- src/main/java/immersivefood/capabilities/CapabilityHandler.java
+++ src/main/java/immersivefood/capabilities/CapabilityHandler.java
@@ -1,12 +1,18 @@
 package immersivefood.capabilities;
 
+import com.google.common.collect.ImmutableList;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import immersivefood.Main;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.List;
 
 @Mod.EventBusSubscriber
 public class CapabilityHandler {
@@ -18,7 +24,18 @@ public class CapabilityHandler {
 		if (event.getObject().getItem() instanceof ItemFood) {
 			long worldTime = Main.proxy().getWorldTime();
 			worldTime = worldTime - worldTime % 20;
-			event.addCapability(FOODDECAY_CAP, new FoodDecayCapability(40, worldTime));
+
+		//	List<Item> list = ImmutableList.copyOf(Item.REGISTRY);
+			ItemFood item = (ItemFood) event.getObject().getItem();
+
+			int healAmount = item.getHealAmount(event.getObject());
+			float saturation = item.getSaturationModifier(event.getObject());
+
+			int baseDecayTime = 1000*20;
+			float multiplier = healAmount * saturation;
+			long decayTime = (long) (baseDecayTime * multiplier);
+			event.addCapability(FOODDECAY_CAP, new FoodDecayCapability(decayTime, worldTime));
+
 		}
 	}
 }
